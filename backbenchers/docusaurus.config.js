8b2e2b3090b15dd462fd10f6072291e360fd7a25
matthewkang <matt0410@students.cs.ubc.ca>
2024-03-15 01:35:20 -0700

    to deploy to another repo

--- backbenchers/docusaurus.config.js
+++ backbenchers/docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://thed3vel0per.github.io/CoastCompanionDemo/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/CoastCompanionDemo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kang-matthew', // Usually your GitHub org/user name.
+  organizationName: 'thed3vel0per', // Usually your GitHub org/user name.
   projectName: 'CoastCompanionDemo', // Usually your repo name.
 
   deploymentBranch: 'gh-pages',
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kang-matthew/CoastCompanionDemo/tree/main/backbenchers/',
+            'https://github.com/thed3vel0per/CoastCompanionDemo/tree/main/backbenchers/',
           //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
@@ -56,7 +56,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kang-matthew/CoastCompanionDemo/tree/main/backbenchers',
+            'https://github.com/thed3vel0per/CoastCompanionDemo/tree/main/backbenchers',
           //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
